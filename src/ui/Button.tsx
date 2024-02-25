09d71c24019e759310eff6fba006ed3662cf53bc
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-02-26 01:14:51 +0200

    style navbar and buttons

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,5 +1,21 @@
-const Button = () => {
-  return <div>Button</div>;
+interface Props {
+  children: string;
+  type ?: string;
+  size ?: string;
+}
+
+const Types: any = {
+  primary: "bg-linearOrange-100 rounded-full",
+};
+
+const Sizes: any = {
+  l: "px-10 py-2" , 
+  xl: "px-14 py-2"
+}
+
+
+const Button = ({ children, type = "primary", size = "l" }: Props) => {
+  return <button className={`${Types[type]} ${Sizes[size]}`}>{children}</button>;
 };
 
 export default Button;
--- src/ui/NavBar.tsx
+++ src/ui/NavBar.tsx
@@ -1,18 +1,20 @@
-import logo from "../assets/logo/logo_color 2.svg";
+import logo from "../assets/logo/logo_color2.png";
 import Button from "./Button";
+
 const NavBar = () => {
   return (
-    <nav className="w-full h-16 flex justify-between bg-gradient-to-r from-linearBlue-3 to-linearBlue-1 via-linearBlue-2 text-white font-poppins font-semibold">
-      <img src={logo} alt="logo" className="w-[80px] h-[80px] p-0" />
-
-      <div className="flex justify-between  w-4/5 m-auto pt-0">
-        <ul className="flex items-center gap-36 pt-0 mt-0">
-          <li>Home</li>
-          <li>Blog</li>
-          <li>Dashboard</li>
-        </ul>
-        <Button />
-      </div>
+    <nav className="w-full h-16 flex justify-between items-center px-24 bg-gradient-to-r from-linearBlue-3 to-linearBlue-1 via-linearBlue-2 text-white font-poppins font-semibold">
+      <img
+        src={logo}
+        alt="logo"
+        className="w-[70px] aspect-[4/3] p-0 object-fill"
+      />
+      <ul className="flex items-center gap-36 pt-0 mt-0">
+        <li>Home</li>
+        <li>Blog</li>
+        <li>Dashboard</li>
+      </ul>
+      <Button>Sign Up</Button>
     </nav>
   );
 };
