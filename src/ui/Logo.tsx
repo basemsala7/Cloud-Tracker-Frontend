6a12f2a452f48893d5df27e20929991660a7a01a
Noran Sherif <>
2024-02-26 02:28:11 +0200

    add aspect ratio to logo

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -3,11 +3,11 @@ import logo from "../assets/logo/logo_color2.png";
 const Logo = () => {
   return (
     <img
-        src={logo}
-        alt="logo"
-        className="w-[60px] h-[40px] p-0 object-fill"
-      />
-  )
-}
+      src={logo}
+      alt="logo"
+      className="w-[70px] aspect-[4/3] h-auto p-0 object-fill"
+    />
+  );
+};
 
-export default Logo
+export default Logo;
