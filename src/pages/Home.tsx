1553be0bd4e8c4d4e806405a17b4d2b3828e8ec0
Shahd Elmeniawy <shahedmohamed180@gmail.com>
2024-02-26 07:19:18 +0200

    import mona-sans font

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,42 +5,42 @@ import leftbannar from "../assets/pen tool.png";
 import Button from "../ui/Button";
 
 const Home = () => {
-  return (
+return (
     <div>
-      <div>
-        <img
-          className="absolute top-20 left-0 w-[150px]"
-          src={leftbannar}
-          alt=""
-        />
-        <img
-          className="absolute top-[-4px] right-0 z-0 w-[900px]"
-          src={bannerbackground}
-          alt=""
-        />
-        <div className="flex justify-between items-center py-28 px-24 z-50">
-          <div className="w-[40%] z-50 flex flex-col justify-between items-center gap-10">
-            <div className="space-y-4">
-              <p className="z-50 text-4xl font-semibold">
-                Your user-friendly guide <br /> to track your
-                <span className="text-linearOrange-100 font-bold"> AWS </span>
-                services !
-              </p>
-              <p className=" text-stone-500">
-                Track and manage your AWS service usage effortlessly.{" "}
-                <span className="font-semibold">Cloud Tracker</span> provides
-                real-time insights and detailed billing information, helping you
-                optimize costs and enhance efficiency. Simplify your AWS
-                experience.
-              </p>
+        <div>
+            <img
+            className="absolute top-20 left-0 w-[150px]"
+            src={leftbannar}
+            alt=""
+            />
+            <img
+            className="absolute top-[-4px] right-0 z-0 w-[900px]"
+            src={bannerbackground}
+            alt=""
+            />
+            <div className="flex justify-between items-center py-28 px-24 z-50">
+                <div className="w-[40%] z-50 flex flex-col justify-between items-center gap-10">
+                    <div className="space-y-4 font-monasans">
+                        <p className="z-50 text-4xl font-semibold">
+                        Your user-friendly guide <br /> to track your
+                        <span className="text-linearOrange-100 font-bold"> AWS </span>
+                        services !
+                        </p>
+                        <p className=" text-stone-500">
+                        Track and manage your AWS service usage effortlessly.{" "}
+                        <span className="font-semibold">Cloud Tracker</span> provides
+                        real-time insights and detailed billing information, helping you
+                        optimize costs and enhance efficiency. Simplify your AWS
+                        experience.
+                        </p>
+                    </div>
+                    <Button size="xl">Get Started</Button>
+                </div>
+                <img className="z-50 w-[650px]" src={bannerimage} alt="" />
             </div>
-            <Button size="xl">Get Started</Button>
-          </div>
-          <img className="z-50 w-[650px]" src={bannerimage} alt="" />
         </div>
-      </div>
     </div>
-  );
+    );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
       },
       fontFamily: {
         poppins: ["Poppins"],
+        monasans: ["mona-sans"],
       },
     },
   },
