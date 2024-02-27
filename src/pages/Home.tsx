9abd93134f185bbd7f0ad20596ac961d36905227
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-02-27 20:58:58 +0200

    some thing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,9 @@ import Button from "../ui/Button";
 const Home = () => {
   return (
     <div>
-      <div>
+
+      {/* first section */}
+      <div className="flex justify-between items-center py-28 px-24 z-50 relative">
         <img
           className="absolute top-20 left-0 w-[150px]"
           src={leftbannar}
@@ -17,26 +19,34 @@ const Home = () => {
           src={bannerbackground}
           alt=""
         />
-        <div className="flex justify-between items-center py-28 px-24 z-50">
-          <div className="w-[45%] z-50 flex flex-col justify-between items-center gap-10">
-            <div className="space-y-4">
-              <p className="z-50 text-4xl font-semibold leading-[50px]">
-                Your user-friendly guide <br /> to track your
-                <span className="text-linearOrange-100 font-bold"> AWS </span>
-                services !
-              </p>
-              <p className=" text-stone-500">
-                Track and manage your AWS service usage effortlessly.{" "}
-                <span className="font-semibold">Cloud Tracker</span> provides
-                real-time insights and detailed billing information, helping you
-                optimize costs and enhance efficiency. Simplify your AWS
-                experience.
-              </p>
-            </div>
-            <Button size="xl">Get Started</Button>
+        <div className="w-[45%] z-50 flex flex-col justify-between items-center gap-10">
+          <div className="space-y-4">
+            <p className="z-50 text-4xl font-semibold leading-[50px]">
+              Your user-friendly guide <br /> to track your
+              <span className="text-linearOrange-100 font-bold"> AWS </span>
+              services !
+            </p>
+            <p className=" text-stone-500">
+              Track and manage your AWS service usage effortlessly.{" "}
+              <span className="font-semibold">Cloud Tracker</span> provides
+              real-time insights and detailed billing information, helping you
+              optimize costs and enhance efficiency. Simplify your AWS
+              experience.
+            </p>
           </div>
-          <img className="z-50 w-[650px]" src={bannerimage} alt="" />
+          <Button size="xl">Get Started</Button>
         </div>
+        <img className="z-50 w-[650px]" src={bannerimage} alt="" />
+      </div>
+
+      {/* second section */}
+      <div>
+
+      </div>
+
+      {/* third section */}
+      <div>
+        
       </div>
     </div>
   );
--- src/ui/Applayout.tsx
+++ src/ui/Applayout.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import NavBar from './NavBar'
-import Footer from './Footer'
-import Home from '../pages/Home'
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Home from "../pages/Home";
 
 const Applayout = () => {
   return (
-      <div>
-          <NavBar />
-          <div className="relative overflow-hidden font-poppins">
-            <Home/>
-          </div>
-          <Footer/>
+    <div>
+      <NavBar />
+      <div className="relative overflow-hidden font-poppins">
+        <Home />
       </div>
-  )
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default Applayout
+export default Applayout;
