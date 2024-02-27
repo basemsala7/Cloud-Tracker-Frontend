3a403717ddee6f8454752e5ce4cb56e8ecf6f390
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-02-27 20:11:59 +0200

    fix navbar

--- src/ui/NavBar.tsx
+++ src/ui/NavBar.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 
 const NavBar = () => {
   return (
-    <nav className="w-full h-16  flex justify-between items-center px-24 bg-gradient-to-r from-linearBlue-3 to-linearBlue-1 via-linearBlue-2 text-white font-poppins font-semibold z-50">
+    <nav className="w-full py-1 flex justify-between items-center px-24 bg-gradient-to-r from-linearBlue-3 to-linearBlue-1 via-linearBlue-2 text-white font-poppins font-semibold z-50">
       <Logo/>
       <ul className="flex items-center gap-36 pt-0 mt-0">
         <li>Home</li>
