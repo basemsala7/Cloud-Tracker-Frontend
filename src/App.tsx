54014a93577ab5284cf04a2b26ff871a4edeccdd
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-04-27 17:45:38 +0300

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import NotFound from "./pages/NotFound";
 import { Profile } from "./pages/Profile";
-// import Applayout from "./ui/Applayout";
 import ScrollToTop from "./ui/ScrollToTop";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
