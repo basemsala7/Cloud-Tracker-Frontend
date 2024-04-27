31c571950b82c6afa2e33da55414e47108948853
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-04-27 09:43:40 +0300

    fix routes path

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import NotFound from "./pages/NotFound";
 import { Profile } from "./pages/Profile";
-// import Applayout from "./ui/Applayout";
+import Applayout from "./ui/Applayout";
 import ScrollToTop from "./ui/ScrollToTop";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -31,13 +31,12 @@ export default function App() {
 					<QueryClientProvider client={queryClient}>
 						<ScrollToTop>
 							<Routes>
-								{/* <Route path="/" element={<Applayout />}> */}
+								<Route path="/" element={<Applayout />}>
 									<Route path="/" element={<Home />} />
-									<Route path="blog" element={<Blog />} />
-									<Route path="blog/:id" element={<BlogDetails />} />
-
+									<Route path="/blog" element={<Blog />} />
+									<Route path="/blog/:id" element={<BlogDetails />} />
 									<Route
-										path="dashboard"
+										path="/dashboard"
 										element={
 											<ProtectRouter route="dashboard">
 												<Dashboard />
@@ -45,14 +44,14 @@ export default function App() {
 										}
 									/>
 									<Route
-										path="profile"
+										path="/profile"
 										element={
 											<ProtectRouter route="profile">
 												<Profile />
 											</ProtectRouter>
 										}
 									/>
-								{/* </Route> */}
+								</Route>
 
 								<Route
 									path="signIn"
