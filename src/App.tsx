a3561287a7b040683aca849323f427563a9f11fc
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-04-27 09:20:21 +0300

    remove applayout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import NotFound from "./pages/NotFound";
 import { Profile } from "./pages/Profile";
-import Applayout from "./ui/Applayout";
+// import Applayout from "./ui/Applayout";
 import ScrollToTop from "./ui/ScrollToTop";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -31,7 +31,7 @@ export default function App() {
 					<QueryClientProvider client={queryClient}>
 						<ScrollToTop>
 							<Routes>
-								<Route path="/" element={<Applayout />}>
+								{/* <Route path="/" element={<Applayout />}> */}
 									<Route path="/" element={<Home />} />
 									<Route path="blog" element={<Blog />} />
 									<Route path="blog/:id" element={<BlogDetails />} />
@@ -52,7 +52,7 @@ export default function App() {
 											</ProtectRouter>
 										}
 									/>
-								</Route>
+								{/* </Route> */}
 
 								<Route
 									path="signIn"
--- src/ui/Applayout.tsx
+++ src/ui/Applayout.tsx
@@ -1,17 +1,19 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-// import Spinner from "./Spinner";
+import Spinner from "./Spinner";
 
 import { Outlet, useLocation } from "react-router-dom";
-// import { useUser } from "../Features/authentication/useUser";
+import { useUser } from "../Features/authentication/useUser";
 
 const Applayout = () => {
 	const loacation = useLocation();
 	const currentPage = loacation.pathname;
 	const setFooter = currentPage === "/" || currentPage === "/profile";
-	// const { isLoading, isAuth } = useUser();
+	const { isLoading, isAuth } = useUser();
 
-	return (
+	return isAuth && isLoading ? (
+		<Spinner />
+	) : (
 		<div>
 			<NavBar />
 			<div
@@ -22,19 +24,6 @@ const Applayout = () => {
 			{setFooter && <Footer />}
 		</div>
 	);
-	// return isAuth && isLoading ? (
-	// 	<Spinner />
-	// ) : (
-	// 	<div>
-	// 		<NavBar />
-	// 		<div
-	// 			className={`relative  ${currentPage === "/blog" ? "h-[calc(100vh-60.5px)]" : ""}  overflow-hidden`}
-	// 		>
-	// 			<Outlet />
-	// 		</div>
-	// 		{setFooter && <Footer />}
-	// 	</div>
-	// );
 };
 
 export default Applayout;
