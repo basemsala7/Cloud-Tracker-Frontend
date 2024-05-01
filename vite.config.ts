82c59e6af9f40f7d2369a3d9a22b2f43b419d438
Basem Salah <74673239+basemsala7@users.noreply.github.com>
2024-05-01 20:49:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/Cloud-Tracker-Frontend/",
+	base: "",
 	test: {
 		globals: true,
 		environment: "jsdom",
